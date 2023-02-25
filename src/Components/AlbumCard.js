0656351a91fd86dac47b68be88e118d8ca49316d
Diego Quiñones <diegoqr2000@gmail.com>
2023-02-25 09:51:45 -0400

    Added functionality to consume the API and show the albums

--- src/Components/AlbumCard.js
+++ src/Components/AlbumCard.js
@@ -5,15 +5,12 @@ import albumDefault from '../Assets/image-album-default.png';
 import './AlbumCard.css';
 
 function AlbumCard(props) {
-    const {image, name, artist, price} = props;
+    const {key, image, name, artist, price} = props;
 
     const defaultSx = {
-        marginLeft: "40px",
-        marginRight: "40px",
         boxShadow: 1,
         borderRadius: 2,
         backgroundColor: 'background.paper',
-        display: 'inline-flex'
         /*
         '&:hover': {
           backgroundColor: 'background.paper',
@@ -23,7 +20,7 @@ function AlbumCard(props) {
     }
 
     return ( 
-        <div data-testid='album-mediacard'>
+        <div data-testid='album-mediacard' key={key}>
             <Box sx={defaultSx}>
                 <div className='box-container'>
                     <div>
@@ -36,7 +33,7 @@ function AlbumCard(props) {
                     <div>
                         <h2>{name}</h2>
                         <h4>{artist}</h4>
-                        {price !== undefined ? <p>{price}</p> : <></>}
+                        {price !== undefined && price !== 0 ? <p>{`${price} $`}</p> : <></>}
                     </div>
                 </div>
             </Box>
--- src/Views/SearchPage.js
+++ src/Views/SearchPage.js
@@ -1,20 +1,33 @@
 import { Button } from '@mui/material';
 import React, { useState } from 'react';
+
 import AlbumCard from '../Components/AlbumCard';
 import ComboBox from '../Components/ComboBox';
 import SearchTextField from '../Components/SearchTextField';
+import useFetch from '../Hooks/useFetch';
+
 import './SearchPage.css'
 
 function SearchPage() {
     
     const [artist, setArtist] = useState('');
-    const [mediaType, setMediaType] = useState('all')
+    const [mediaType, setMediaType] = useState('all');
+    const [searchResults, setSearchResults] = useState([]);
+
+    const { get, loading } = useFetch("https://itunes.apple.com/search?")
 
     const options = ['movie', 'podcast', 'music', 'musicVideo', 'audiobook', 'shortFilm', 'tvShow', 'software', 'ebook', 'all'];
 
     function handleSearchClick(){
-        console.log(artist);
-        console.log(mediaType);
+        var artistURL = `term=${artist.toLowerCase().replace(' ', '+')}`;
+        var mediaURL = `entity=${mediaType}`;
+        var url = mediaType === 'all' ? artistURL : `${artistURL}&${mediaURL}`
+        get(url)
+        .then(data => {
+            console.log(data);
+            setSearchResults(data.results)
+        })
+        .catch(error => console.log(error))
     }
 
     return ( 
@@ -24,7 +37,14 @@ function SearchPage() {
                 <ComboBox id='media-type-combobox' label='Tipo de media' value={mediaType} onChange={(event, newValue) => {setMediaType(newValue)}} options={options} variant='outlined'/>
                 <Button variant='outlined' onClick={handleSearchClick}>Buscar</Button>
             </div>
-            <AlbumCard name="Lost Heaven" artist="Vincent Footlose" price="14.99$"/>
+            <div className='search-results'>
+                {
+                    searchResults.map((album) => {
+                        return <AlbumCard key={album.collectionId} image={album.artworkUrl100} name={album.trackName} artist={album.artistName} price={album.collectionPrice} />
+                    })
+                }
+            </div>
+            
         </div>
     );
 }
